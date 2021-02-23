12c189fafa29426195c96b2967f138196520c7ee
lisa-glitch <lisaoloo001@gmail.com>
2021-02-23 14:00:09 +0300

    add weekdays

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function akan() {
+  var birthDate = document.getElementById("date").value;
+  var gender = document.getElementById("gender").value;
+  console.log("birthDate");
+  if (!birthDate) {
+    alert("You entered an invalid date.");
+  }
+}
+var birthMonth;
+birthMonth = parseInt(birthDate.slice(5, 7));
+var birthDay;
+birthDay = parseInt(birthDate.slice(8, 10));
+var year = parseInt(birthDate.slice(0, 4));
+if (birthDay <= O || birthDay > 31 || birthMonth <= 0 || birthMonth > 12) {
+  alert("You entered an invalid date.");
+}
